79296d0067c60eac36fef78c7338aa3d81e0ccb4
3Cat-jaguar <ryusa13@gmail.com>
2021-10-09 04:13:40 +0900

    edit setting

--- newTestProject/settings.py
+++ newTestProject/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(7dr*(567xbl*u$goyli9v+1j@c7xeflm0j2($n3@wppgy=%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-#    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,4 +137,4 @@ MEDIA_URL = '/media/'
 
 FILE_UPLOAD_MAX_MEMORY_SIZE = '2621440'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
