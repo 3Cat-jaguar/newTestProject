b8604e9ebe160e9990ca9d5fb11c5bcd9aca19a0
3Cat-jaguar <ryusa13@gmail.com>
2021-10-09 04:12:47 +0900

    find ota edit procfile

--- newTestProject/settings.py
+++ newTestProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(7dr*(567xbl*u$goyli9v+1j@c7xeflm0j2($n3@wppgy=%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 ALLOWED_HOSTS = []
