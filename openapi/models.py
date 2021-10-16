838c2dee29cdcab6e1b9de8f66f52427ccac3091
3Cat-jaguar <ryusa13@gmail.com>
2021-10-16 20:15:10 +0900

    start new app for using openapi

---
+++ openapi/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class api_table(models.Model):
+    item_name = models.CharField(max_length=30)
+    kind_name = models.CharField(max_length=30)
+    rank = models.CharField(max_length=30)
+    unit = models.CharField(max_length=30)
+    date = models.CharField(max_length=30)
+    today_price = models.CharField(max_length=30)
+    average_price = models.CharField(max_length=30)
