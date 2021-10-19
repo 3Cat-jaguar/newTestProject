b530e31c9afcdc2162f014e63cbdf39874b43887
3Cat-jaguar <ryusa13@gmail.com>
2021-10-19 12:27:54 +0900

    Feat: add openapi request, save data to db

--- Tables/views.py
+++ Tables/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 #from .models import post_table
 from openapi.models import api_table
 import requests
+import datetime
+from datetime import datetime, date
 
 
-def parsing(request):
-    date = str('2021-10-05')
-    category = str(100)
+def request_openapi_data(urldate, category):
     requesturl = "https://www.kamis.or.kr/service/price/xml.do?action=dailyPriceByCategoryList"
     requesturl += "&p_product_cls_code=01"
-    requesturl += "&p_regday=" + date
+    requesturl += "&p_regday=" + urldate
     requesturl += "&p_convert_kg_yn=N"
     requesturl += "&p_item_category_code=" + category
     requesturl += "&p_cert_key=111"
@@ -18,30 +18,102 @@ def parsing(request):
     print('request url : ' + str(requesturl))
 
     apidata = requests.get(requesturl)
+    # request 실패시 처리를 어떻게 할지?
     jsondata = apidata.json()
-    if (api_table.objects.all().exists()==False):
-        for item in jsondata['data']['item']:
-            item_name = item['item_name']
-            #print(item_name + item['unit'])
-            api_table.objects.create(category = category, item_name = item_name,
-                                     kind_name = item['kind_name'], rank = item['rank'],
-                                     unit = item['unit'], date = item['day1'],
-                                     today_price = item['dpr1'], average_price = item['dpr7'])
-    elif (api_table.objects.all().first().date != jsondata['data']['item'][0]['day1']):
-        index = api_table.objects.all().first().id
-        for item in jsondata['data']['item']:
-            objects = api_table.objects.get(id=index)
+    return jsondata
+
+
+def create_db(urldate, i):
+    category = str(i * 100)
+    jsondata = request_openapi_data(urldate, category)
+    for item in jsondata['data']['item']:
+        # print(item_name + item['unit'])
+        api_table.objects.create(category=category, item_name=item['item_name'],
+                                 kind_name=item['kind_name'], rank=item['rank'],
+                                 unit=item['unit'], date=item['day1'],
+                                 today_price=item['dpr1'], average_price=item['dpr7'])
+    return
+
+
+def update_db(urldate, i, index):
+    category = str(i * 100)
+    jsondata = request_openapi_data(urldate, category)
+    print(index)
+    for item in jsondata['data']['item']:
+        print(index)
+        objectslist = api_table.objects.filter(id=index)
+        if objectslist:
+            print('데이터를 수정합니다')
+            objects = objectslist.first()
+            objects.category = category
+            objects.item_name = item['item_name']
+            objects.kind_name = item['kind_name']
+            objects.rank = item['rank']
+            objects.unit = item['unit']
             objects.date = item['day1']
             if (item['dpr1'] != '-'):
                 objects.today_price = item['dpr1']
+            elif (item['dpr2'] != '-'):
+                objects.today_price = item['dpr2']
+            elif (item['dpr3'] != '-'):
+                objects.today_price = item['dpr3']
+            elif (item['dpr4'] != '-'):
+                objects.today_price = item['dpr4']
+            elif (item['dpr5'] != '-'):
+                objects.today_price = item['dpr5']
+            else:
+                objects.today_price = '-'
             objects.average_price = item['dpr7']
             objects.save()
+        else:
+            print('데이터를 추가합니다')
+            api_table.objects.create(category=category, item_name=item['item_name'],
+                                     kind_name=item['kind_name'], rank=item['rank'],
+                                     unit=item['unit'], date=item['day1'],
+                                     today_price=item['dpr1'], average_price=item['dpr7'])
+        index += 1
+    return index
+
+
+def parsing(request):
+    todayyear = datetime.now().year
+    todaymonth = datetime.now().month
+    todaydate = datetime.now().day
+    todayweekday = str(date(todayyear, todaymonth, todaydate).strftime('%A'))
+    print('today weekday : ' + todayweekday)
+    urldate = str(todayyear) + '-' + str(todaymonth) + '-' + str(todaydate)
+    dbdate = '당일 (' + str(todaymonth) + '/' + str(todaydate) + ')'
+    print(urldate)
+    if not api_table.objects.all().exists():
+        i = 1
+        while i < 5:
+            create_db(urldate, i)
+            i += 1
+    elif (api_table.objects.all().first().date != dbdate) and (todayweekday != 'Sunday'):
+        # 일요일 외 nodata 가 나오는 다른 케이스도 찾아야함
+        index = api_table.objects.all().first().id
+        i = 1
+        while i < 5:
+            index = update_db(urldate, i, index)
+            i += 1
+        objectslist = api_table.objects.filter(id=index)
+        while objectslist:
+            print('데이터 초기화')
+            objects = objectslist.first()
+            objects.category = 0
+            objects.item_name = ''
+            objects.kind_name = ''
+            objects.rank = ''
+            objects.unit = ''
+            objects.date = ''
+            objects.today_price = ''
+            objects.average_price = ''
+            objects.save()
             index += 1
-
+            objectslist = api_table.objects.filter(id=index)
     context = {
-        'apidata':api_table.objects.all()
+        'apidata':api_table.objects.all().exclude(item_name='').order_by('id')
     }
-
     return render(request, 'index.html', context)
 
 
